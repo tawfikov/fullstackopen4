b4499495f8f89f94edff7766a5ed220c6cc60a17
Marwan Tawfik <marwantawfik2048@gmail.com>
2025-09-20 18:32:55 +0300

    ex 4.2

---
+++ blogList/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config.js')
+const logger = require('./utils/logger.js')
+const blogsRouter = require('./controllers/blogs.js')
+
+const app = express()
+
+logger.info('Connecting to MongoDB..')
+mongoose.connect(config.MONGODB_URI)
+    .then(()=>{
+        logger.info('connected to MongoDB')
+    })
+    .catch((error)=>{
+        logger.error('An error happend while connecting to MongoDB: ', error.message)
+    })
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+ module.exports = app
